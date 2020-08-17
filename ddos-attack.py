adf65a409a096abeac96afc9cbe1e41b7dafabf6
mehul21-lab <69211144+mehul21-lab@users.noreply.github.com>
2020-08-17 16:34:15 +0530

    me

--- ddos-attack.py
+++ ddos-attack.py
@@ -24,7 +24,6 @@ print "Author   : HA-MRX"
 print "You Tube : https://www.youtube.com/c/HA-MRX"
 print "github   : https://github.com/Ha3MrX"
 print "Facebook : https://www.facebook.com/muhamad.jabar222"
-print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
